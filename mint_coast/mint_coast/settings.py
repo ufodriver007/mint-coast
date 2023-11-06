ae340e91596bdb7d02f71b2a5706f39da76a8fe5
ufodriver007 <nemo_122@mail.ru>
2023-11-06 15:53:47 +0300

    edited setting

--- mint_coast/mint_coast/settings.py
+++ mint_coast/mint_coast/settings.py
@@ -163,7 +163,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
