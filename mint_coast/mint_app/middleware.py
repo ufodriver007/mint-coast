ee54e9f232f95913abfb76653ccff422770fbca1
ufodriver007 <nemo_122@mail.ru>
2023-12-07 13:20:31 +0300

    added ip blacklist(as file)

--- mint_coast/mint_app/middleware.py
+++ mint_coast/mint_app/middleware.py
@@ -5,6 +5,7 @@ from datetime import datetime
 from pytz import timezone
 from django.contrib.auth import logout
 from django.core.cache import cache
+from django.conf import settings
 
 
 class BanMiddleware:
@@ -31,6 +32,9 @@ class ThrottlingMiddleware:
         # Получаем IP-адрес пользователя или другую уникальную строку для идентификации
         # здесь используется IP-адрес, но это не самое надежное решение
         client_ip = request.META['REMOTE_ADDR']
+
+        if client_ip in settings.BLACKLIST:
+            return HttpResponseForbidden('Ваш IP заблокирован!')
 
         # Устанавливаем ключ для кэша, используя IP-адрес и префикс
         cache_key = f'throttle_{client_ip}'
--- mint_coast/mint_coast/settings.py
+++ mint_coast/mint_coast/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 
 
 load_dotenv()
+
+BLACKLIST = {}
+with open('blacklist') as f:
+    BLACKLIST = {item.strip(): '' for item in f}
+
 DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
